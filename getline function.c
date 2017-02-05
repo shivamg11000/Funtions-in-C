27f8f3e64420285565ed49483a22c0cb7d734e58
shivamg11000 <shivamg11000code@gmail.com>
2017-02-05 11:42:18 +0530

    Add files via upload
    getlin function which reads a line to an array at a time including newline character.

---
+++ getline function.c
@@ -0,0 +1,21 @@
+/* function reads a line to s[] including newline charcter */   
+
+
+#include<stdio.h>
+int getlin(char s[], int lim) /* lim is max length of input line  */ 
+{
+  int c, i;
+
+  for( i=0 ; i<lim-1 && (c=getchar())!=EOF && c!='\n'; i++)/* first it is checked whether there    */
+  {  s[i] = c;                                             /* is space in array, then whether it   */ 
+  }                                                        /* is end of file or paragraph,         */
+                                                           /* then whether it is newline character */
+  if( c=='\n' )                                                            
+  { s[i++] = c;
+  }
+
+  s[i] = '\0';              /* assigns null characerter at end of s[] */
+
+  return i;                 /* returns the size of input */ 
+
+}
